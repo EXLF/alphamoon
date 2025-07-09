15381a33b855dc49cfd26467401175c417c9eb7f
EXLF <your.email@example.com>
2025-07-09 13:31:28 +0800

    新增pm2配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // 允许外部访问
+    port: 5173,       // 指定端口
+    strictPort: true, // 端口被占用时不要尝试下一个端口
+  }
 })
